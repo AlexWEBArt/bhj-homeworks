33799a7e1125dafae3741c989296c093c0733965
AlexWEBArt <elusiv@mail.ru>
2020-09-20 23:40:03 +0500

    finished

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,12 @@
+
+const counter = document.getElementById("clicker__counter");
+const image = document.getElementById("cookie");
+image.onclick = function() {
+    if (image.width == 200) {
+        image.width = 250;
+    }   else {
+        image.width = 200;
+    }
+    counter.textContent -= -1;
+};
+    
