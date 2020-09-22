0168777c73a3303bc1df4f1c37eb39c9e3707041
AlexWEBArt <elusiv@mail.ru>
2020-09-22 22:25:29 +0500

    fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,11 @@
 
 const change = function() {
-    const clock = document.getElementById("timer")
+    const clock = document.getElementById("timer");
     if (clock.textContent != 0) {
-        clock.textContent -= 1
+        clock.textContent -= 1;
     } else {
         alert('Вы победили в конкурсе!');
-    }
-} 
-setInterval(change, 1000)
+        clock.textContent = 3;
+    };
+};
+setInterval(change, 1000);
